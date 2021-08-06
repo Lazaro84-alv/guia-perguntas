2d339465cdbfc407d66de60ea92eb3477ea023ca
Lázaro Alves <geo.eolo@gmail.com>
2021-08-05 22:17:21 -0300

    Adiciona o bootstrap ao projeto

--- index.js
+++ index.js
@@ -2,16 +2,10 @@ const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
-app.get("/:nome/:lang", (req,res) => {
-  var nome = req.params.nome;
-  var lang = req.params.lang;
-  res.render("index", {
-    nome: nome,
-    lang: lang,
-    empresa: "Guia do programador",
-    inscritos: 8000
-  });
+app.get("/", (req,res) => {
+  res.render("index");
 });
 
 app.listen(8080, () => {
