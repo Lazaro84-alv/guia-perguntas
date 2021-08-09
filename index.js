58c650ca783d7892390f34f55eb16c9fb3c183ba
Lázaro Alves <geo.eolo@gmail.com>
2021-08-09 17:37:10 -0300

    Adiciona front-end a página de pergunta

--- index.js
+++ index.js
@@ -49,7 +49,9 @@ app.get("/pergunta/:id",(req,res) => {
     where: {id: id}
   }).then(pergunta => {
     if(pergunta != undefined){
-      res.render("pergunta");
+      res.render("pergunta",{
+        pergunta: pergunta
+      });
     }else{
       res.redirect("/");
     }
